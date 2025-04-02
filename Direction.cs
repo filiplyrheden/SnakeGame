dab8620de3ac803e9d1e7f894aee7196157e5075
filiplyrheden <filip.lyrheden@gmail.com>
2025-04-02 13:31:11 +0200

    Wrote code for Directions class

--- Direction.cs
+++ Direction.cs
@@ -1,6 +1,53 @@
+using System;
+
 namespace SnakeGame;
 
 public class Direction
 {
-    
+
+ public readonly static Direction Left = new Direction(0, -1);
+ public readonly static Direction Right = new Direction(0, 1);
+ public readonly static Direction Up = new Direction(-1, 0);
+ public readonly static Direction Down = new Direction(1, 0);
+ 
+ public int RowOffset { get; }   
+ public int ColOffset { get; }
+
+ private Direction(int rowOffset, int colOffset)
+ {
+  RowOffset = rowOffset;
+  ColOffset = colOffset;
+ }
+
+ public Direction Opposite()
+ {
+  return new Direction(-RowOffset, -ColOffset);
+ }
+ protected bool Equals(Direction other)
+ {
+  return RowOffset == other.RowOffset && ColOffset == other.ColOffset;
+ }
+
+ public override bool Equals(object obj)
+ {
+  if (obj is null) return false;
+  if (ReferenceEquals(this, obj)) return true;
+  if (obj.GetType() != GetType()) return false;
+  return Equals((Direction)obj);
+ }
+
+ public override int GetHashCode()
+ {
+  return HashCode.Combine(RowOffset, ColOffset);
+ }
+
+ public static bool operator ==(Direction left, Direction right)
+ {
+  return Equals(left, right);
+ }
+
+ public static bool operator !=(Direction left, Direction right)
+ {
+  return !Equals(left, right);
+ }
 }
