8d21292dc956515d4f0ee671a66fa19095294e5a
filiplyrheden <filip.lyrheden@gmail.com>
2025-04-18 15:20:24 +0200

    Added code for drawing game state

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,16 +1,39 @@
+using System;
+using System.Collections.Generic;
 using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.Media;
 
 namespace SnakeGame;
 
 public partial class MainWindow : Window
 {
+    private readonly Dictionary<GridValue, IImage> gridValToImage = new()
+    {
+        { GridValue.Empty, Images.Empty },
+        { GridValue.Snake, Images.Body },
+        { GridValue.Food, Images.Food }
+    };
     
     private readonly int rows = 15, cols = 15;
     private readonly Image[,] gridImages;
+    private GameState gameState;
     public MainWindow()
     {
         InitializeComponent();
         gridImages = SetupGrid();
+        gameState = new GameState(rows, cols);
+    }
+
+    private void Window_Opened(object? sender, EventArgs e)
+    {
+        Draw();
+    }
+
+    private void Window_KeyDown(object? sender, KeyEventArgs e)
+    {
+        // Handle arrow keys here
     }
 
     private Image[,] SetupGrid()
@@ -34,5 +57,23 @@ public partial class MainWindow : Window
         }
         
         return images;
+        
+    }
+
+    private void Draw()
+    {
+        DrawGrid();
+    }
+
+    private void DrawGrid()
+    {
+        for (int r = 0; r < rows; r++)
+        {
+            for (int c = 0; c < cols; c++)
+            {
+                GridValue gridVal = gameState.Grid[r, c];
+                gridImages[r, c].Source = gridValToImage[gridVal];
+            }
+        }
     }
 }
